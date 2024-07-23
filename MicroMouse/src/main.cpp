725fa4a03a6b4ca44f9e9eb7351ddda741b9fbe6
reda <reda1609>
2024-07-24 00:46:28 +0300

    updates for testing

--- MicroMouse/src/main.cpp
+++ MicroMouse/src/main.cpp
@@ -12,12 +12,14 @@ DC_MOTOR motor1 = DC_MOTOR(input1, input2, enable, encoder1, encoder2);
 PID pid = PID(0.45, 0.0025, 0.001);
 
 void setup() {
+    Serial.begin(9600);
     motor1.init();
-    pid.setSetpoint(1000);
+    pid.setSetpoint(2000);
 }
 
 void loop() {
     int feedback = motor1.get_pos_feedback();
+    Serial.println(feedback);
     int speed = pid.compute(feedback);
     pid.direction > 0 ? motor1.forward(speed) : motor1.backward(speed);
 }
