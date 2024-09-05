655f7574e158f9e07ab5382c823c1043bae5ca6f
reda1609 <ahmedredasorour12@gmail.com>
2024-09-05 20:13:04 +0300

    velocity control

--- MicroMouse/src/main.cpp
+++ MicroMouse/src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 #include "ROBOT.h"
+#include "PID.h"
 // #include "FloodFill.h"
-// #include <DC_MOTOR.h>
 
 ROBOT robot = ROBOT(9.7, 6.5, 970);
+
+PID right_velocity_pid = PID(0.56, 0.01, 0.0);
+PID left_velocity_pid = PID(0.4, 0.01, 0.0);
 
 unsigned long last_update_time = 0;
 
@@ -26,8 +29,9 @@ void setup()
   // robot.move_distance(19.2);
   // robot.rotate_angle(90); 
   
-  robot.right_motor.forward(100);
-  robot.left_motor.forward(100);
+  right_velocity_pid.setSetpoint(1000);
+  left_velocity_pid.setSetpoint(1000);
+
   unsigned long last_update_time = millis();
 }
 
@@ -40,9 +44,17 @@ void loop()
   if (current_time - last_update_time >= 100) {
       robot.right_motor.update_velocity_1(current_time);
       robot.left_motor.update_velocity_2(current_time);
+      right_velocity_pid.setFeedback(robot.right_motor.get_velocity_1());
+      left_velocity_pid.setFeedback(robot.left_motor.get_velocity_2());
+      int speed1 = right_velocity_pid.compute();
+      int speed2 = left_velocity_pid.compute();
+      right_velocity_pid.direction > 0 ? 
+        robot.right_motor.forward(speed1) : robot.right_motor.backward(speed1);
+      left_velocity_pid.direction > 0 ? 
+        robot.left_motor.forward(speed2) : robot.left_motor.backward(speed2); 
       Serial.print("left motor: ");
       Serial.print(robot.left_motor.get_velocity_2());
-      Serial.print('\t');
+      Serial.print("\t\t");
       Serial.print("right right: ");
       Serial.println(robot.right_motor.get_velocity_1());
       last_update_time = current_time;
