62116499200992b4cba9649d5249c921415f4ffc
reda1609 <ahmedredasorour12@gmail.com>
2024-09-07 14:39:13 +0300

    update move distance function

--- MicroMouse/lib/ROBOT/ROBOT.h
+++ MicroMouse/lib/ROBOT/ROBOT.h
@@ -30,13 +30,19 @@ public:
     PID imu_pid = PID(3,0.02, 0.0, 255);
     DC_MOTOR right_motor = DC_MOTOR(input1_1, input2_1, enable_1, encoder1_1, encoder2_1);
     DC_MOTOR left_motor =  DC_MOTOR(input1_2, input2_2, enable_2, encoder1_2, encoder2_2);
+
+    PID right_velocity_pid = PID(0.565, 0.0, 0.0, 255);
+    PID left_velocity_pid = PID(0.38, 0.0, 0.0, 255);
+    PID right_pos_pid = PID(3.2, 0.0, 0.0, 2000);
+    PID left_pos_pid = PID(3, 0.0, 0.0, 2000);
+
     IMU2040 imu = IMU2040();
     double wheelSeparation, wheelDiameter, encoder1_prev_error, encoder2_prev_error;
     double imu_prev_error;
     int PPR; // pulse per revolution
-    unsigned long stopping_time, prev_time;
+    unsigned long stopping_time, prev_time, last_update_time = 0;
     unsigned long imu_stopping_time, imu_prev_time;
-    int repeat = 0;
+    int repeat = 0, right_pos_setpoint = 0, left_pos_setpoint = 0;
 
 public:
     ROBOT(double wheelSeparation, double wheelDiameter, int PPR)
@@ -48,6 +54,7 @@ public:
     {
         // while (!Serial)
         //     ;
+        
         if (!imu.init())
         {
             Serial.println("IMU initialization failed!");
@@ -65,27 +72,38 @@ public:
         double circumference = 3.14 * wheelDiameter;
         double rotations_no = distance / circumference;
         int pulses = rotations_no * PPR;
-        repeat++;
-        encoder1_pid.setSetpoint(pulses * repeat);
-        encoder2_pid.setSetpoint(pulses * repeat);
+        right_pos_setpoint += pulses;
+        left_pos_setpoint += pulses;
+        right_pos_pid.setSetpoint(right_pos_setpoint);
+        left_pos_pid.setSetpoint(left_pos_setpoint);
         prev_time = millis();
         while (millis() - prev_time < stopping_time)
         {
-            encoder1_pid.setFeedback(right_motor.get_pos_feedback_1());
-            encoder2_pid.setFeedback(left_motor.get_pos_feedback_2());
-            int speed1= encoder1_pid.compute();
-            int speed2= encoder2_pid.compute();
-            Serial.print("left motor = ");
-            Serial.print(encoder1_pid.getFeedback());
-            Serial.print('\t');
-            Serial.print("right motor = ");
-            Serial.println(encoder2_pid.getFeedback());
-
-            encoder1_pid.direction > 0 ? right_motor.forward(speed1) : right_motor.backward(speed1);
-            encoder2_pid.direction > 0 ? left_motor.forward(speed2) : left_motor.backward(speed2);           
+            unsigned long current_time = millis();
+            if (current_time - last_update_time >= 30) {
+                right_motor.update_velocity_1(current_time);
+                left_motor.update_velocity_2(current_time);
+                right_pos_pid.setFeedback(right_motor.get_pos_feedback_1());
+                left_pos_pid.setFeedback(left_motor.get_pos_feedback_2());
+                right_velocity_pid.setSetpoint(right_pos_pid.compute());
+                left_velocity_pid.setSetpoint(left_pos_pid.compute());
+                right_velocity_pid.setFeedback(right_motor.get_velocity_1());
+                left_velocity_pid.setFeedback(left_motor.get_velocity_2());
+                int speed1 = right_velocity_pid.compute();
+                int speed2 = left_velocity_pid.compute();
+                right_pos_pid.direction > 0 ? 
+                right_motor.forward(speed1) : right_motor.backward(speed1);
+                left_pos_pid.direction > 0 ? 
+                left_motor.forward(speed2) : left_motor.backward(speed2); 
+                Serial.print("right motor: ");
+                Serial.print(left_motor.get_pos_feedback_2());
+                Serial.print("\t\t");
+                Serial.print("left right: ");
+                Serial.println(right_motor.get_pos_feedback_1());
+                last_update_time = current_time;
+            }           
         }
         this->stop();
-        delay(100);
     }
 
     void rotate_angle(double angle)
--- MicroMouse/src/main.cpp
+++ MicroMouse/src/main.cpp
@@ -1,48 +1,48 @@
 #include <Arduino.h>
 #include "ROBOT.h"
-#include "PID.h"
+// #include "PID.h"
 // #include "FloodFill.h"
 
 // pos_setpoint -> (912) -> position -> (0,2000) -> velocity -> (0,255) -> motor
 
 ROBOT robot = ROBOT(9.7, 6.5, 970);
 
-PID right_velocity_pid = PID(0.565, 0.0, 0.0, 255);
-PID left_velocity_pid = PID(0.38, 0.0, 0.0, 255);
+// PID right_velocity_pid = PID(0.565, 0.0, 0.0, 255);
+// PID left_velocity_pid = PID(0.38, 0.0, 0.0, 255);
 
-PID right_pos_pid = PID(3, 0.0, 0.0, 2000);
-PID left_pos_pid = PID(3, 0.0, 0.0, 2000);
+// PID right_pos_pid = PID(3, 0.0, 0.0, 2000);
+// PID left_pos_pid = PID(3, 0.0, 0.0, 2000);
 
-unsigned long last_update_time = 0;
-unsigned long repeat_time = 0;
-int repeat = 0;
+// unsigned long last_update_time = 0;
+// unsigned long repeat_time = 0;
+// int repeat = 0;
 
-void move_forward(){
-  unsigned long current_time = millis();
+// void move_forward(){
+//   unsigned long current_time = millis();
 
-  if (current_time - last_update_time >= 30) {
-    robot.right_motor.update_velocity_1(current_time);
-    robot.left_motor.update_velocity_2(current_time);
-    right_pos_pid.setFeedback(robot.right_motor.get_pos_feedback_1());
-    left_pos_pid.setFeedback(robot.left_motor.get_pos_feedback_2());
-    right_velocity_pid.setSetpoint(right_pos_pid.compute());
-    left_velocity_pid.setSetpoint(left_pos_pid.compute());
-    right_velocity_pid.setFeedback(robot.right_motor.get_velocity_1());
-    left_velocity_pid.setFeedback(robot.left_motor.get_velocity_2());
-    int speed1 = right_velocity_pid.compute();
-    int speed2 = left_velocity_pid.compute();
-    right_pos_pid.direction > 0 ? 
-      robot.right_motor.forward(speed1) : robot.right_motor.backward(speed1);
-    left_pos_pid.direction > 0 ? 
-      robot.left_motor.forward(speed2) : robot.left_motor.backward(speed2); 
-    Serial.print("right motor: ");
-    Serial.print(robot.left_motor.get_pos_feedback_2());
-    Serial.print("\t\t");
-    Serial.print("left right: ");
-    Serial.println(robot.right_motor.get_pos_feedback_1());
-    last_update_time = current_time;
-  }
-}
+//   if (current_time - last_update_time >= 30) {
+//     robot.right_motor.update_velocity_1(current_time);
+//     robot.left_motor.update_velocity_2(current_time);
+//     right_pos_pid.setFeedback(robot.right_motor.get_pos_feedback_1());
+//     left_pos_pid.setFeedback(robot.left_motor.get_pos_feedback_2());
+//     right_velocity_pid.setSetpoint(right_pos_pid.compute());
+//     left_velocity_pid.setSetpoint(left_pos_pid.compute());
+//     right_velocity_pid.setFeedback(robot.right_motor.get_velocity_1());
+//     left_velocity_pid.setFeedback(robot.left_motor.get_velocity_2());
+//     int speed1 = right_velocity_pid.compute();
+//     int speed2 = left_velocity_pid.compute();
+//     right_pos_pid.direction > 0 ? 
+//       robot.right_motor.forward(speed1) : robot.right_motor.backward(speed1);
+//     left_pos_pid.direction > 0 ? 
+//       robot.left_motor.forward(speed2) : robot.left_motor.backward(speed2); 
+//     Serial.print("right motor: ");
+//     Serial.print(robot.left_motor.get_pos_feedback_2());
+//     Serial.print("\t\t");
+//     Serial.print("left right: ");
+//     Serial.println(robot.right_motor.get_pos_feedback_1());
+//     last_update_time = current_time;
+//   }
+// }
 
 void setup()
 {
@@ -51,10 +51,13 @@ void setup()
   // }
 
   Serial.begin(9600);
-  robot.right_motor.init();
-  robot.left_motor.init();
+
+  // robot.right_motor.init();
+  // robot.left_motor.init();
+
   // initialize();
-  // robot.init();
+
+  robot.init();
   
   // for(int i = 0; i < 3; i++){
   //   robot.move_distance(19.2);
@@ -66,41 +69,41 @@ void setup()
   // right_velocity_pid.setSetpoint(2000);
   // left_velocity_pid.setSetpoint(2000);
 
-  right_pos_pid.setSetpoint(912);
-  left_pos_pid.setSetpoint(912);
+  // right_pos_pid.setSetpoint(912);
+  // left_pos_pid.setSetpoint(912);
 
-  unsigned long last_update_time = millis();
-  unsigned long repeat_time = millis();
+  // unsigned long last_update_time = millis();
+  // unsigned long repeat_time = millis();
 }
 
  
 
 void loop()
 {
-  move_forward();
+  // move_forward();
 
-  if(millis() - repeat_time > 2000){
-    repeat++;
-    if(repeat > 5){
-      repeat = 1;
-      robot.right_motor.reset_pos_1();
-      robot.left_motor.reset_pos_2();
-    }
-    if(repeat <= 3){
-      right_pos_pid.setSetpoint(912*repeat);
-      left_pos_pid.setSetpoint(912*repeat);
-    }
-    else if(repeat > 3){
-      robot.right_motor.reset_pos_1();
-      robot.left_motor.reset_pos_2();
-      right_pos_pid.setSetpoint(-(362));
-      left_pos_pid.setSetpoint(380);
-    }
-    repeat_time = millis();
-  }
+  // if(millis() - repeat_time > 2000){
+  //   repeat++;
+  //   if(repeat > 5){
+  //     repeat = 1;
+  //     robot.right_motor.reset_pos_1();
+  //     robot.left_motor.reset_pos_2();
+  //   }
+  //   if(repeat <= 3){
+  //     right_pos_pid.setSetpoint(912*repeat);
+  //     left_pos_pid.setSetpoint(912*repeat);
+  //   }
+  //   else if(repeat > 3){
+  //     robot.right_motor.reset_pos_1();
+  //     robot.left_motor.reset_pos_2();
+  //     right_pos_pid.setSetpoint(-(362));
+  //     left_pos_pid.setSetpoint(380);
+  //   }
+  //   repeat_time = millis();
+  // }
   
   // solve();
-  // robot.move_distance(19.2);
+  robot.move_distance(19.2);
   // robot.Rotaion_move_imu(90);
   // robot.rotate_angle(90);  
 }
