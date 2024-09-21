78540c21e70c2c79b9e9085714968ee10732f3c6
reda1609 <ahmedredasorour12@gmail.com>
2024-09-21 20:38:34 +0300

    some code reduction

--- MicroMouse/src/main.cpp
+++ MicroMouse/src/main.cpp
@@ -38,15 +38,6 @@ const uint16_t  ALL_SENSORS_PENDING = ((1 << COUNT_SENSORS) - 1);
 uint16_t        sensors_pending = ALL_SENSORS_PENDING;
 uint32_t        sensor_last_cycle_time;
 
-
-/*
-    Reset all sensors by setting all of their XSHUT pins low for delay(10), then set all XSHUT high to bring out of reset
-    Keep sensor #1 awake by keeping XSHUT pin high
-    Put all other sensors into shutdown by pulling XSHUT pins low
-    Initialize sensor #1 with lox.begin(new_i2c_address) Pick any number but 0x29 and it must be under 0x7F. Going with 0x30 to 0x3F is probably OK.
-    Keep sensor #1 awake, and now bring sensor #2 out of reset by setting its XSHUT pin high.
-    Initialize sensor #2 with lox.begin(new_i2c_address) Pick any number but 0x29 and whatever you set the first sensor to
-*/
 void setID() {
   // all reset
   digitalWrite(SHT_LOX1, LOW);
@@ -80,8 +71,6 @@ void setID() {
     while (1);
   }
   lox2.setAddress(LOX2_ADDRESS);
-
- 
 }
 
 void readSensor(Adafruit_VL6180X &vl) {
@@ -134,168 +123,8 @@ void read_sensors() {
 
 }
 
-void timed_read_sensors() {
-  digitalWrite(TIMING_PIN, HIGH);
-  uint32_t start_time = millis();
-  uint8_t range_lox1 = lox1.readRange();
-  uint8_t status_lox1 = lox1.readRangeStatus();
-  uint8_t range_lox2 = lox2.readRange();
-  uint8_t status_lox2 = lox2.readRangeStatus();
-  uint32_t delta_time = millis() - start_time;
-  digitalWrite(TIMING_PIN, LOW);
-  Serial.print(delta_time, DEC);
-  Serial.print(" : ");
-  if (status_lox1 == VL6180X_ERROR_NONE) Serial.print(range_lox1, DEC);
-  else Serial.print("###");
-  Serial.print(" : ");
-  if (status_lox2 == VL6180X_ERROR_NONE) Serial.print(range_lox2, DEC);
-  else Serial.print("###");
-  Serial.print(" : ");
-  Serial.println();
-}
-
-void timed_async_read_sensors() {
-  digitalWrite(TIMING_PIN, HIGH);
-  uint32_t start_time = millis();
-
-  // lets start up all three
-  lox1.startRange();
-  lox2.startRange();
-  // wait for each of them to complete
-  lox1.waitRangeComplete();
-  lox2.waitRangeComplete();
-  uint8_t range_lox1 = lox1.readRangeResult();
-  uint8_t status_lox1 = lox1.readRangeStatus();
-  uint8_t range_lox2 = lox2.readRangeResult();
-  uint8_t status_lox2 = lox2.readRangeStatus();
-  uint32_t delta_time = millis() - start_time;
-  digitalWrite(TIMING_PIN, LOW);
-  Serial.print(delta_time, DEC);
-  Serial.print(" : ");
-  if (status_lox1 == VL6180X_ERROR_NONE) Serial.print(range_lox1, DEC);
-  else Serial.print("###");
-  Serial.print(" : ");
-  if (status_lox2 == VL6180X_ERROR_NONE) Serial.print(range_lox2, DEC);
-  else Serial.print("###");
-  Serial.print(" : ");
-
-  Serial.println();
-
-}
-
-void timed_async_read_gpio() {
-#ifdef GPIO_LOX1
-  uint8_t range_lox1 = 0;
-  uint8_t status_lox1 = VL6180X_ERROR_NONE;
-  uint8_t range_lox2 = 0;
-  uint8_t status_lox2 = VL6180X_ERROR_NONE;
-
-  digitalWrite(TIMING_PIN, HIGH);
-  uint8_t pending_sensors = 0x7;
-  uint32_t start_time = millis();
-
-  // lets start up all three
-  lox1.startRange();
-  lox2.startRange();
-
-  while (pending_sensors && ((millis() - start_time) < 1000)) {
-    if ((pending_sensors & 0x1) && !digitalRead(GPIO_LOX1)) {
-      range_lox1 = lox1.readRangeResult();
-      status_lox1 = lox1.readRangeStatus();
-      pending_sensors ^= 0x1;
-    }
-    if ((pending_sensors & 0x2) && !digitalRead(GPIO_LOX2)) {
-      range_lox2 = lox2.readRangeResult();
-      status_lox2 = lox2.readRangeStatus();
-      pending_sensors ^= 0x2;
-    }
-   
-  }
-  uint32_t delta_time = millis() - start_time;
-  digitalWrite(TIMING_PIN, LOW);
-  Serial.print(delta_time, DEC);
-  Serial.print("(");
-  Serial.print(pending_sensors, DEC);
-  Serial.print(") : ");
-  if (status_lox1 == VL6180X_ERROR_NONE) Serial.print(range_lox1, DEC);
-  else Serial.print("###");
-  Serial.print(" : ");
-  if (status_lox2 == VL6180X_ERROR_NONE) Serial.print(range_lox2, DEC);
-  else Serial.print("###");
-  Serial.print(" : ");
-  
-  Serial.println();
-#endif
-}
-
-//===============================================================
-// Continuous range test code
-//===============================================================
-void start_continuous_range() {
-  Serial.println("start Continuous range mode");
-  for (uint8_t i = 0; i < COUNT_SENSORS; i++) {
-    sensors[i]->startRangeContinuous();
-  }
-  sensors_pending = ALL_SENSORS_PENDING;
-  sensor_last_cycle_time = millis();
-}
-
-void stop_continuous_range() {
-  Serial.println("Stop Continuous range mode");
-  for (uint8_t i = 0; i < COUNT_SENSORS; i++) {
-    sensors[i]->stopRangeContinuous();
-  }
-  delay(100); // give time for it to complete.
-}
-
-uint8_t sensor_gpios[10]={0};
-void Process_continuous_range() {
-
-  uint16_t mask = 1;
-  for (uint8_t i = 0; i < COUNT_SENSORS; i++) {
-    bool range_complete = false;
-    if (sensors_pending & mask) {
-      if (sensor_gpios[i] >= 0)
-        range_complete = !digitalRead(sensor_gpios[i]);
-      else
-        range_complete = sensors[i]->isRangeComplete();
-      if (range_complete) {
-        sensor_ranges[i] = sensors[i]->readRangeResult();
-        sensor_status[i] = sensors[i]->readRangeStatus();
-        sensors_pending ^= mask;
-      }
-    }
-    mask <<= 1; // setup to test next one
-  }
-  // See if we have all of our sensors read OK
-  uint32_t delta_time = millis() - sensor_last_cycle_time;
-  if (!sensors_pending || (delta_time > 1000)) {
-    Serial.print(delta_time, DEC);
-    Serial.print("(");
-    Serial.print(sensors_pending, HEX);
-    Serial.print(")");
-    mask = 1;
-    for (uint8_t i = 0; i < COUNT_SENSORS; i++) {
-      Serial.print(" : ");
-      if (sensors_pending & mask) Serial.print("TTT");  // show timeout in this one
-      else if (sensor_status[i] == VL6180X_ERROR_NONE) Serial.print(sensor_ranges[i], DEC);
-      else {
-        Serial.print("#");
-        Serial.print(sensor_status[i], DEC);
-      }
-    }
-    // setup for next pass
-    Serial.println();
-    sensor_last_cycle_time = millis();
-    sensors_pending = ALL_SENSORS_PENDING;
-  }
-}
-
 IMU2040 imu = IMU2040();
 
-//===============================================================
-// Setup
-//===============================================================
 void setup() {
   Serial.begin(115200);
 
@@ -310,8 +139,6 @@ void setup() {
   // Enable timing pin so easy to see when pass starts and ends
   pinMode(TIMING_PIN, OUTPUT);
 
-
-
   Serial.println("Shutdown pins inited...");
 
   digitalWrite(SHT_LOX1, LOW);
@@ -320,12 +147,10 @@ void setup() {
   digitalWrite(TIMING_PIN, LOW);
   Serial.println("All in reset mode...(pins are low)");
 
-
   Serial.println("Starting...");
   setID();
 
   imu.init();
-
 }
 
 void loop() {
@@ -337,10 +162,11 @@ void loop() {
     Serial.print(" :");
     Serial.print(sensor_ranges[i]);
     Serial.print("mm");
-    Serial.println();
+    Serial.print('\t');
   }
 
   imu.calulations();
   Serial.print("yaw: ");
   Serial.println(imu.get_Yaw_angle());
+  Serial.println("============================================");
 }
