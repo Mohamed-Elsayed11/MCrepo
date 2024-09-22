f06a3d4d3b5a0900bf9cbc4887e5488397065274
reda1609 <ahmedredasorour12@gmail.com>
2024-09-22 23:32:30 +0300

    more code simplification

--- MicroMouse/src/main.cpp
+++ MicroMouse/src/main.cpp
@@ -2,169 +2,48 @@
 #include <Adafruit_VL6180X.h>
 #include "IMU.h"
 
-MbedI2C I2C1(A0, A1); // This is your custom I2C bus
+MbedI2C I2C1(A0, A1);
 
-// address we will assign if dual sensor is present
-#define LOX1_ADDRESS 0x30
-#define LOX2_ADDRESS 0x31
+#define TOF1_ADDRESS 0x30
+#define TOF2_ADDRESS 0x31
 
-// set the pins to shutdown
-#define SHT_LOX1 2
-#define SHT_LOX2 3
+#define SHUT1 A2
+#define SHUT2 A3
 
-#define TIMING_PIN 13
-
-// objects for the VL6180X
-Adafruit_VL6180X lox1 = Adafruit_VL6180X();
-Adafruit_VL6180X lox2 = Adafruit_VL6180X();
-
-// Setup mode for doing reads
-typedef enum {RUN_MODE_DEFAULT, RUN_MODE_TIMED, RUN_MODE_ASYNC, RUN_MODE_GPIO, RUN_MODE_CONT} runmode_t;
-uint8_t tempRange=0;
-runmode_t run_mode = RUN_MODE_DEFAULT;
-uint8_t show_command_list = 1;
-
-//==========================================================================
-// Define some globals used in the continuous range mode
-// Note: going to start table drive this part, may back up and do the rest later
-Adafruit_VL6180X *sensors[] = {&lox1, &lox2};
-const uint8_t COUNT_SENSORS = sizeof(sensors) / sizeof(sensors[0]);
-//const int sensor_gpios[COUNT_SENSORS] = {GPIO_LOX1, GPIO_LOX2}; // if any are < 0 will poll instead
-
-uint8_t         sensor_ranges[COUNT_SENSORS];
-uint8_t         sensor_status[COUNT_SENSORS];
-// Could do with uint8_t for 8 sensors, but just in case...
-const uint16_t  ALL_SENSORS_PENDING = ((1 << COUNT_SENSORS) - 1);
-uint16_t        sensors_pending = ALL_SENSORS_PENDING;
-uint32_t        sensor_last_cycle_time;
-
-void setID() {
-  // all reset
-  digitalWrite(SHT_LOX1, LOW);
-  digitalWrite(SHT_LOX2, LOW);
-  delay(10);
-
-  // all unreset
-  digitalWrite(SHT_LOX1, HIGH);
-  digitalWrite(SHT_LOX2, HIGH);
-  delay(10);
-
-  // activating LOX1 and reseting LOX2
-  digitalWrite(SHT_LOX1, HIGH);
-  digitalWrite(SHT_LOX2, LOW);
-
-  // initing LOX1
-  if (!lox1.begin(&I2C1)) {
-    Serial.println(F("Failed to boot first VL6180X"));
-    while (1);
-  }
-  lox1.setAddress(LOX1_ADDRESS);
-  delay(10);
-
-  // activating LOX2
-  digitalWrite(SHT_LOX2, HIGH);
-  delay(10);
-
-  //initing LOX2
-  if (!lox2.begin(&I2C1)) {
-    Serial.println(F("Failed to boot second VL6180X"));
-    while (1);
-  }
-  lox2.setAddress(LOX2_ADDRESS);
-}
-
-void readSensor(Adafruit_VL6180X &vl) {
-  
-  float lux = vl.readLux(VL6180X_ALS_GAIN_5);
-  uint8_t range = vl.readRange();
-  uint8_t status = vl.readRangeStatus();
-
-  if (status == VL6180X_ERROR_NONE) {
-     tempRange=range;
-  }
-
-  // Some error occurred, print it out!
-
-  if  ((status >= VL6180X_ERROR_SYSERR_1) && (status <= VL6180X_ERROR_SYSERR_5)) {
-    Serial.print("(System error)");
-  }
-  else if (status == VL6180X_ERROR_ECEFAIL) {
-    Serial.print("(ECE failure)");
-  }
-  else if (status == VL6180X_ERROR_NOCONVERGE) {
-    Serial.print("(No convergence)");
-  }
-  else if (status == VL6180X_ERROR_RANGEIGNORE) {
-    Serial.print("(Ignoring range)");
-  }
-  else if (status == VL6180X_ERROR_SNR) {
-    Serial.print("Signal/Noise error");
-  }
-  else if (status == VL6180X_ERROR_RAWUFLOW) {
-    Serial.print("Raw reading underflow");
-  }
-  else if (status == VL6180X_ERROR_RAWOFLOW) {
-    Serial.print("Raw reading overflow");
-  }
-  else if (status == VL6180X_ERROR_RANGEUFLOW) {
-    Serial.print("Range reading underflow");
-  }
-  else if (status == VL6180X_ERROR_RANGEOFLOW) {
-    Serial.print("Range reading overflow");
-  }
-}
-
-void read_sensors() {
-  readSensor(lox1);
-  sensor_ranges[0]=tempRange;
-  readSensor(lox2);
-  sensor_ranges[1]=tempRange;
-  Serial.println();
-
-}
+Adafruit_VL6180X right_TOF = Adafruit_VL6180X();
+Adafruit_VL6180X left_TOF = Adafruit_VL6180X();
 
 IMU2040 imu = IMU2040();
 
+void setID() {
+  digitalWrite(SHUT1, HIGH);
+  digitalWrite(SHUT2, LOW);
+
+  right_TOF.begin(&I2C1);
+  right_TOF.setAddress(TOF1_ADDRESS);
+  delay(10);
+  digitalWrite(SHUT2, HIGH);
+  delay(10);
+
+  left_TOF.begin(&I2C1);
+  left_TOF.setAddress(TOF2_ADDRESS);
+}
+
 void setup() {
   Serial.begin(115200);
-
-  // wait until serial port opens for native USB devices
-  while (! Serial) {
-    delay(1);
-  }
-
-  pinMode(SHT_LOX1, OUTPUT);
-  pinMode(SHT_LOX2, OUTPUT);
-
-  // Enable timing pin so easy to see when pass starts and ends
-  pinMode(TIMING_PIN, OUTPUT);
-
-  Serial.println("Shutdown pins inited...");
-
-  digitalWrite(SHT_LOX1, LOW);
-  digitalWrite(SHT_LOX2, LOW);
-
-  digitalWrite(TIMING_PIN, LOW);
-  Serial.println("All in reset mode...(pins are low)");
-
-  Serial.println("Starting...");
+  pinMode(SHUT1, OUTPUT);
+  pinMode(SHUT2, OUTPUT);
   setID();
-
   imu.init();
 }
 
 void loop() {
-  for(int i = 0; i < 100; i++){
-    read_sensors();
-    for(int i=0;i<COUNT_SENSORS;i++)
-    {
-      Serial.print("Sensor");
-      Serial.print(i);
-      Serial.print(" :");
-      Serial.print(sensor_ranges[i]);
-      Serial.print("mm");
-      Serial.print('\t');
-    }
+  for(int i = 0; i < 1000; i++){
+    Serial.print("sensor 1: ");
+    Serial.print(right_TOF.readRange());
+    Serial.print('\t');
+    Serial.print("sensor 2: ");
+    Serial.print(left_TOF.readRange());
     Serial.print("\n");
   }
 
